cae425c1ab13de19fea2fb89c6a43d7dbf047fe1
Eimantas <59708220+EimantasGag@users.noreply.github.com>
2024-10-01 21:47:37 +0300

    Pridejau user endpoint, kad gauti useri pagal jo id (#3)
    * Get user endpointas

--- features/User/Interfaces/IUserService.cs
+++ features/User/Interfaces/IUserService.cs
@@ -3,5 +3,7 @@ using Features.User.Entities;
 public interface IUserService
 {
     Task<IEnumerable<User>> GetAllUsersAsync();
+
+    Task<User?> GetUserAsync(Guid userID);
 }
 
--- obj/Debug/net8.0/ReflexSharp-BE.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReflexSharp-BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994570be1117fadab5cb3fc5954a316ed219fb35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a0d17252cc4e64ab4577023e159563eb7c153b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
