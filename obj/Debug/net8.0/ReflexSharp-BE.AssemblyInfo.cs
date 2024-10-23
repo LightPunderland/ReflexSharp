d62b0671e69d111386bf0e79fcc53ed90bedb6fc
Lukas Jasiulionis <lukas.jasiulionis@mif.stud.vu.lt>
2024-10-23 22:52:24 +0300

    Added audio file management to backend

--- obj/Debug/net8.0/ReflexSharp-BE.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReflexSharp-BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ba1757e80d997144289ab6aa79593553fb5c19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183307756840f0a7b3c3f20df0311f01c1aa878e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
