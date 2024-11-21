5e26baa0f97ba43975063be14f634e3a7cc167be
nojusbuc <noahmb.nmb@gmail.com>
2024-11-21 15:39:15 +0200

    xp/gold visible, adjusted response from posting xp/gold

--- obj/Debug/net8.0/ReflexSharp-BE.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReflexSharp-BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a02031828ac28f7258958eab35f8a735f75aae6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41a94f004302da40eaa5b6933572dd0faa6ffb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReflexSharp-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
